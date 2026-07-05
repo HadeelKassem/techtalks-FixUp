f6f02e5486398e6eca01218336b696a836824a26
Hadi ibrahim <Hadi5ibrahim10@gmail.com>
2026-07-05 18:18:05 +0300

    Last changes before testing on Bokking feature , Modified some syntaxs and bugs

--- fixup-backend/src/main/java/com/fixup/repository/CategoryRepository.java
+++ fixup-backend/src/main/java/com/fixup/repository/CategoryRepository.java
@@ -1,5 +1,11 @@
 package com.fixup.repository;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fixup.model.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
--- fixup-backend/src/main/java/com/fixup/repository/ServiceRequestRepository.java
+++ fixup-backend/src/main/java/com/fixup/repository/ServiceRequestRepository.java
@@ -1,5 +1,16 @@
 package com.fixup.repository;
 
-public interface ServiceRequestRepository {
+import com.fixup.model.ServiceRequest;
+import com.fixup.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ServiceRequestRepository extends JpaRepository<ServiceRequest, Long> {
+    List<ServiceRequest> findByClient(User client);
+    List<ServiceRequest> findByProvider(User provider);
+    List<ServiceRequest> findByStatus(ServiceRequest.RequestStatus status);
+    List<ServiceRequest> findByClientAndStatus(User client, ServiceRequest.RequestStatus status);
 
 }
