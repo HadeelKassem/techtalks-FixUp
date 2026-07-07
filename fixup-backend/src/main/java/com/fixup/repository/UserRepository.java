1e406157b08c165b6e8899413558b94d175504cd
Hadi ibrahim <Hadi5ibrahim10@gmail.com>
2026-07-07 17:07:45 +0300

    delete dublicates imports

--- fixup-backend/src/main/java/com/fixup/repository/UserRepository.java
+++ fixup-backend/src/main/java/com/fixup/repository/UserRepository.java
@@ -8,11 +8,6 @@ import org.springframework.stereotype.Repository;
 import com.fixup.model.User;
 
 @Repository
-import com.fixup.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
